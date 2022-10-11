ab314fe7dc99a715bd1b67fb327a4359ae3f0866
LENOVO <LENOVO@DESKTOP-02MC7HS>
2022-10-11 00:22:22 -0500

    Agregar archivos de proyecto.

---
+++ PAR IMPAR 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Programiii
+    {
+        static void Main(string[]args)
+        {
+            // Determinar si un número es Par o Impar en C#
+            Console.Write("Introduce un número: ");
+            int num = int.Parse(Console.ReadLine()); 
+
+             
+            if(num % 2 == 0)
+            {
+                Console.Write("El número " + num + " es Par.");
+            }
+            else
+            {
+                Console.Write("El número " + num + " es Impar.");
+            }
+        }
+    }
+}
+
+
+    
+
